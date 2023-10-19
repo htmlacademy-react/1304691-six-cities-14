e19cec0f22b45ae345e610c623711ed3b61525c6
Divashka <dianakuzmina325@gmail.com>
2023-10-19 17:23:10 +0300

    3.13. Маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main/main';
+import NotFound from '../../pages/404/404';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   cardCount: number;
@@ -6,7 +12,32 @@ type AppScreenProps = {
 
 function App({ cardCount }: AppScreenProps): JSX.Element {
   return (
-    <MainPage cardCount={cardCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage cardCount={cardCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
