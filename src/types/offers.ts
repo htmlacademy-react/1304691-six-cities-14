ce3955bc60d2ca0ddf1adf0160dc73aa945a780e
Divashka <dianakuzmina325@gmail.com>
2023-10-29 23:29:29 +0300

    Описала псевдонимы типов

---
+++ src/types/offers.ts
@@ -0,0 +1,41 @@
+type Host = {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+}
+
+type City = {
+  location: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+  };
+  name: string;
+}
+
+type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export type Offer = {
+  bedrooms: number;
+  city: City;
+  description: string;
+  goods: string[];
+  host: Host;
+  id: number;
+  images: string[];
+  isFavorite: boolean;
+  isPremium: boolean;
+  location: Location;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+}
+
