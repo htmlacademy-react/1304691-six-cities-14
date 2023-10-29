5b8bed4264a76e0deed4c1d393060c4ae729484e
Divashka <dianakuzmina325@gmail.com>
2023-10-29 23:21:06 +0300

    4.1. Добавит моки для офферов и отзывов

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,64 @@
+const reviews = [
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Sun Oct 29 2023 15:06:43 GMT+0300 (Москва, стандартное время)',
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: 'img/1.png',
+      id: 1,
+      isPro: false,
+      name: 'Oliver.conner'
+    }
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Sun Oct 29 2023 15:06:43 GMT+0300 (Москва, стандартное время)',
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: 'img/1.png',
+      id: 1,
+      isPro: false,
+      name: 'Oliver.conner'
+    }
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Sun Oct 29 2023 15:06:43 GMT+0300 (Москва, стандартное время)',
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: 'img/1.png',
+      id: 1,
+      isPro: false,
+      name: 'Oliver.conner'
+    }
+  }
+  , {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Sun Oct 29 2023 15:06:43 GMT+0300 (Москва, стандартное время)',
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: 'img/1.png',
+      id: 1,
+      isPro: false,
+      name: 'Oliver.conner'
+    }
+  },
+  {
+    comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    date: 'Sun Oct 29 2023 15:06:43 GMT+0300 (Москва, стандартное время)',
+    id: 1,
+    rating: 4,
+    user: {
+      avatarUrl: 'img/1.png',
+      id: 1,
+      isPro: false,
+      name: 'Oliver.conner'
+    }
+  }
+];
+
+export { reviews };
