6f60d6ad84064c22df1e827b030ae188cc451f46
Divashka <dianakuzmina325@gmail.com>
2023-10-29 23:31:46 +0300

    Обучила card принимать и отображать данные

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,21 +1,32 @@
-function Card() {
+import { Offer } from '../../types/offers';
+import { RATING_MAX } from '../../const';
+
+type CardProps = {
+  offerInfo: Offer;
+  onMouseOver: React.MouseEventHandler;
+}
+
+function Card({ offerInfo, onMouseOver }: CardProps): JSX.Element {
+
+  const { price, title, rating, previewImage, isPremium, isFavorite, type, id } = offerInfo;
+
+  const ratingValue = (rating * 100) / RATING_MAX;
+
   return (
-    <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+    <article className="cities__card place-card" data-id={id} onMouseOver={onMouseOver}>
+      {isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"></img>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"></img>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={`place-card__bookmark-button button ${isFavorite ? 'place-card__bookmark-button--active ' : ''}`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -24,14 +35,14 @@ function Card() {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: `${ratingValue}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
