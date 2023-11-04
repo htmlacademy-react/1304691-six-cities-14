aa7f7d4bb5c0b363d5ce2d1d4cabbb50052d6db0
Divashka <dianakuzmina325@gmail.com>
2023-11-04 16:11:41 +0300

    Моки для города

---
+++ src/mocks/map.ts
@@ -0,0 +1,15 @@
+import { City } from '../types/types';
+
+const CITY: City = {
+  location: {
+    latitude: 52.37022,
+    longitude: 4.89517,
+    zoom: 10
+  },
+  name: 'Amsterdam',
+};
+
+export {
+  CITY
+};
+
