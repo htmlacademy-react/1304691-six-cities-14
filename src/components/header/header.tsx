8ceaa6107904133fdf2e0724f7bf20fa34823846
Divashka <dianakuzmina325@gmail.com>
2023-11-23 15:01:59 +0300

    поправила хедер

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,13 @@
 import { Link } from 'react-router-dom';
 import Logo from '../logo/logo';
 import { AppRoute } from '../../const';
+import { useAppSelector } from '../../hooks';
+import { AuthorizationStatus } from '../../const';
 
 function Header(): JSX.Element {
+
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
   return (
     <header className="header">
       <div className="container">
@@ -14,18 +19,28 @@ function Header(): JSX.Element {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
-                <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile" >
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </Link>
+                {authorizationStatus === AuthorizationStatus.Auth
+                  ?
+                  <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile" >
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    <span className="header__favorite-count">3</span>
+                  </Link>
+                  :
+                  <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__login">Sign in</span>
+                  </Link>}
               </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
+              {authorizationStatus === AuthorizationStatus.Auth
+                &&
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>}
             </ul>
           </nav>
         </div>
