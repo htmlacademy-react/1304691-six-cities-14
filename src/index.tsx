cf0543416b48bfbb8c73bd746f4ad5378cd75357
Divashka <dianakuzmina325@gmail.com>
2023-11-27 09:39:09 +0300

    Стили для тостера

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { fetchOffersAction, checkAuthAction } from './store/api-actions';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 store.dispatch(checkAuthAction());
 store.dispatch(fetchOffersAction());
