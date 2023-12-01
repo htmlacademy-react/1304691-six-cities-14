95c839c7bf9a2e6f45a48bf0c317aa120f80228f
Divashka <dianakuzmina325@gmail.com>
2023-12-01 17:25:23 +0300

    Тесты для асинхр операций 2

--- src/store/data-process/data-process.test.ts
+++ src/store/data-process/data-process.test.ts
@@ -3,9 +3,9 @@ import MockAdapter from 'axios-mock-adapter';
 import thunk from 'redux-thunk';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { Action } from 'redux';
-import { AppThunkDispatch, extractActionsTypes, makeFakeOffer } from '../../utils/mocks';
-import { State } from '../../types/types';
-import { checkAuthAction, fetchOffersAction } from '../api-actions';
+import { AppThunkDispatch, extractActionsTypes, makeFakeOffer, makeFakeFavoriteOffer, makeFakeReview, fakeOffer } from '../../utils/mocks';
+import { OfferPreview, Review, State } from '../../types/types';
+import { checkAuthAction, fetchOffersAction, fetchFavoritesAction, fetchAroundOffersAction, fetchReviewsAction, fetchOfferAction, fetchAddToFavoriteAction } from '../api-actions';
 import { APIRoute } from '../../const';
 
 describe('Async actions', () => {
@@ -16,8 +16,18 @@ describe('Async actions', () => {
 
   let store: ReturnType<typeof mockStoreCreator>;
 
+  let mockOffers: OfferPreview[];
+  let mockFavoriteOffers: OfferPreview[];
+  let mockReviews: Review[];
+
   beforeEach(() => {
     store = mockStoreCreator({ DATA: { offers: [] } });
+
+    mockOffers = new Array(5).fill(null).map(() => makeFakeOffer());
+
+    mockFavoriteOffers = new Array(5).fill(null).map(() => makeFakeFavoriteOffer());
+
+    mockReviews = new Array(5).fill(null).map(() => makeFakeReview());
   });
 
   describe('checkAuthAction', () => {
@@ -50,11 +60,10 @@ describe('Async actions', () => {
 
   describe('fetchOffersAction', () => {
 
-    it('should dispatch fetchOffersAction.pending, fetchOffersAction.fulfilled when servet response 200', async () => {
-      const mockOffers = new Array(5).fill(null).map(() => makeFakeOffer());
+    it('should dispatch fetchOffersAction.pending, fetchOffersAction.fulfilled when server response 200', async () => {
+      mockAxiosAdapter.onGet(APIRoute.Offers).reply(200, mockOffers);
 
       await store.dispatch((fetchOffersAction()));
-      mockAxiosAdapter.onGet(APIRoute.Offers).reply(200, mockOffers);
 
       const emittedActions = store.getActions();
       const extractedActionsTypes = extractActionsTypes(emittedActions);
@@ -69,17 +78,125 @@ describe('Async actions', () => {
         .toEqual(mockOffers);
     });
 
-    it('should dispatch fetchOffersAction.pending, fetchOffersAction.rejected when servet response 400', async () => {
-      mockAxiosAdapter.onGet(APIRoute.Offers).reply(400, []);
+  });
 
-      await store.dispatch((fetchOffersAction()));
-      const actions = extractActionsTypes(store.getActions());
+  describe('fetchFavoriteOffersAction', () => {
 
-      expect(actions).toEqual([
-        fetchOffersAction.pending.type,
-        fetchOffersAction.rejected.type,
+    it('should dispatch fetchFavoriteOffersAction.pending, fetchFavoriteOffersAction.fulfilled when server response 200', async () => {
+      mockAxiosAdapter.onGet(APIRoute.Favorite).reply(200, mockFavoriteOffers);
+
+      await store.dispatch((fetchFavoritesAction()));
+
+      const emittedActions = store.getActions();
+      const extractedActionsTypes = extractActionsTypes(emittedActions);
+      const fetchFavoritesOffersActionFulfilled = emittedActions.at(1) as ReturnType<typeof fetchFavoritesAction.fulfilled>;
+
+      expect(extractedActionsTypes).toEqual([
+        fetchFavoritesAction.pending.type,
+        fetchFavoritesAction.fulfilled.type,
       ]);
 
+      expect(fetchFavoritesOffersActionFulfilled.payload)
+        .toEqual(mockFavoriteOffers);
+    });
+
+  });
+
+  describe('fetchAroundOffersAction', () => {
+
+    it('should dispatch fetchAroundOffersAction.pending, fetchAroundOffersAction.fulfilled when server response 200', async () => {
+      const id = '6af6f711-c28d-4121-82cd-e0b462a27f00';
+      mockAxiosAdapter.onGet(`${APIRoute.Offers}/${id}/nearby`).reply(200, mockOffers);
+
+      await store.dispatch((fetchAroundOffersAction(id)));
+
+      const emittedActions = store.getActions();
+      const extractedActionsTypes = extractActionsTypes(emittedActions);
+      const fetchAroundOffersActionFulfilled = emittedActions.at(1) as ReturnType<typeof fetchAroundOffersAction.fulfilled>;
+
+      expect(extractedActionsTypes).toEqual([
+        fetchAroundOffersAction.pending.type,
+        fetchAroundOffersAction.fulfilled.type,
+      ]);
+
+      expect(fetchAroundOffersActionFulfilled.payload)
+        .toEqual(mockOffers);
+    });
+
+  });
+
+  describe('fetchReviewsAction', () => {
+
+    it('should dispatch fetchReviewsAction.pending, fetchReviewsAction.fulfilled when server response 200', async () => {
+      const id = '6af6f711-c28d-4121-82cd-e0b462a27f00';
+      mockAxiosAdapter.onGet(`${APIRoute.Comments}/${id}`).reply(200, mockReviews);
+
+      await store.dispatch((fetchReviewsAction(id)));
+
+      const emittedActions = store.getActions();
+      const extractedActionsTypes = extractActionsTypes(emittedActions);
+      const fetchReviewsActionFulfilled = emittedActions.at(1) as ReturnType<typeof fetchReviewsAction.fulfilled>;
+
+      expect(extractedActionsTypes).toEqual([
+        fetchReviewsAction.pending.type,
+        fetchReviewsAction.fulfilled.type,
+      ]);
+
+      expect(fetchReviewsActionFulfilled.payload)
+        .toEqual(mockReviews);
+    });
+
+  });
+
+  describe('fetchOfferAction', () => {
+
+    it('should dispatch fetchOfferAction.pending, fetchOfferAction.fulfilled when server response 200', async () => {
+      const id = '6af6f711-c28d-4121-82cd-e0b462a27f00';
+      mockAxiosAdapter.onGet(`${APIRoute.Offers}/${id}`).reply(200, fakeOffer);
+
+      await store.dispatch((fetchOfferAction(id)));
+
+      const emittedActions = store.getActions();
+      const extractedActionsTypes = extractActionsTypes(emittedActions);
+      const fetchOfferActionFulfilled = emittedActions.at(1) as ReturnType<typeof fetchOfferAction.fulfilled>;
+
+      expect(extractedActionsTypes).toEqual([
+        fetchOfferAction.pending.type,
+        fetchOfferAction.fulfilled.type,
+      ]);
+
+      expect(fetchOfferActionFulfilled.payload)
+        .toEqual(fakeOffer);
+    });
+
+  });
+
+  describe('fetchAddToFavoriteAction', () => {
+
+    it('should dispatch fetchAddToFavoriteAction.pending, fetchAddToFavoriteAction.fulfilled when server response 200', async () => {
+      const id = '6af6f711-c28d-4121-82cd-e0b462a27f00';
+      const status = 0;
+
+      const addToFavoritesData = {
+        id: id,
+        status: 0
+      };
+
+      mockAxiosAdapter.onGet(`${APIRoute.Favorite}/${id}/${status}`).reply(200, fakeOffer);
+
+      await store.dispatch((fetchAddToFavoriteAction(addToFavoritesData)));
+
+      const emittedActions = store.getActions();
+      const extractedActionsTypes = extractActionsTypes(emittedActions);
+      const fetchAddToFavoriteActionFulfilled = emittedActions.at(1) as ReturnType<typeof fetchAddToFavoriteAction.fulfilled>;
+
+      expect(extractedActionsTypes).toEqual([
+        fetchAddToFavoriteAction.pending.type,
+        fetchAddToFavoriteAction.fulfilled.type,
+      ]);
+
+      expect(fetchAddToFavoriteActionFulfilled.payload)
+        .toEqual(fakeOffer);
     });
 
   });
--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,9 +1,8 @@
 import { ThunkDispatch } from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
 import { Action } from 'redux';
-import { OfferPreview, State } from '../types/types';
+import { OfferPreview, State, Review } from '../types/types';
 import { CityName } from '../const';
-
 
 export type AppThunkDispatch = ThunkDispatch<State, ReturnType<typeof createAPI>, Action>;
 
@@ -32,3 +31,76 @@ export const makeFakeOffer = (): OfferPreview => ({
   rating: 4,
   previewImage: 'https://url-to-image/image.png'
 });
+
+export const makeFakeFavoriteOffer = (): OfferPreview => ({
+  id: '6af6f711-c28d-4121-82cd-e0b462a27f00',
+  title: 'Beautiful & luxurious studio at great location',
+  type: 'apartment',
+  price: 120,
+  city: {
+    name: CityName.Amsterdam,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    }
+  },
+  location: {
+    latitude: 52.35514938496378,
+    longitude: 4.673877537499948,
+    zoom: 8
+  },
+  isFavorite: true,
+  isPremium: false,
+  rating: 4,
+  previewImage: 'https://url-to-image/image.png'
+});
+
+export const makeFakeReview = (): Review => ({
+  id: 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+  date: '2019-05-08T14:13:56.569Z',
+  user: {
+    name: 'Oliver Conner',
+    avatarUrl: 'https://url-to-image/image.png',
+    isPro: false
+  },
+  comment: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+  rating: 4
+});
+
+export const fakeOffer = {
+  id: '6af6f711-c28d-4121-82cd-e0b462a27f00',
+  title: 'Beautiful & luxurious studio at great location',
+  type: 'apartment',
+  price: 120,
+  city: {
+    name: CityName.Amsterdam,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    }
+  },
+  location: {
+    latitude: 52.35514938496378,
+    longitude: 4.673877537499948,
+    zoom: 8
+  },
+  isFavorite: false,
+  isPremium: false,
+  rating: 4,
+  description: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+  bedrooms: 3,
+  goods: [
+    'Heating'
+  ],
+  host: {
+    name: 'Oliver Conner',
+    avatarUrl: 'https://url-to-image/image.png',
+    isPro: false
+  },
+  images: [
+    'https://url-to-image/image.png'
+  ],
+  maxAdults: 4
+};
